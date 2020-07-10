a4e779a6c09b0580089514225d31bfc99b26029e
lindexi <lindexi_gd@163.com>
2020-07-10 11:16:51 +0800

    修改获取文件

--- src/dotnetCampus.OfficeDocumentZipper/MainWindow.xaml.cs
+++ src/dotnetCampus.OfficeDocumentZipper/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows;
+﻿using System.Diagnostics;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace dotnetCampus.OfficeDocumentZipper
 {
@@ -10,6 +13,82 @@ namespace dotnetCampus.OfficeDocumentZipper
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void OpenOfficeFile_OnClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void UnZip_OnClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Explorer_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(OfficeFolder.Text))
+            {
+                Warn($"OfficeFolder can not be empty");
+                return;
+            }
+
+            if (!Directory.Exists(OfficeFolder.Text))
+            {
+                Warn($"Office Folder {OfficeFolder.Text} not found");
+            }
+
+            Process.Start(new ProcessStartInfo("explorer")
+            {
+                ArgumentList =
+                {
+                    OfficeFolder.Text
+                }
+            });
+        }
+
+        private void Zip_OnClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Warn(string text)
+        {
+            if (Dispatcher.CheckAccess())
+            {
+                Warning.Text = text;
+            }
+            else
+            {
+                Dispatcher.InvokeAsync(() =>
+                {
+                    Warning.Text = text;
+                });
+            }
+        }
+
+        private void OfficeFile_OnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            var file = GetFile();
+
+            OfficeFolder.Text = Path.GetDirectoryName(file) ?? string.Empty;
+        }
+
+        private string GetFile()
+        {
+            var file = OfficeFile.Text;
+            if (file.StartsWith("\""))
+            {
+                file = file.Substring(1);
+            }
+
+            if (file.EndsWith("\""))
+            {
+                file = file.Remove(file.Length - 1);
+            }
+
+            file = Path.GetFullPath(file);
+            return file;
         }
     }
 }
