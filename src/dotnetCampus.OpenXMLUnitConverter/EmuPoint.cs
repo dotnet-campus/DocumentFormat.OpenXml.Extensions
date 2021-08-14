bac8c1b1bd6c2a8108b01b8813f497bf3abe13f6
lindexi <lindexi_gd@163.com>
2021-08-14 11:17:17 +0800

    加上点的定义

---
+++ src/dotnetCampus.OpenXMLUnitConverter/EmuPoint.cs
@@ -0,0 +1,29 @@
+﻿namespace dotnetCampus.OpenXmlUnitConverter
+{
+    /// <summary>
+    /// 采用 <see cref="Emu"/> 表示的点
+    /// </summary>
+    public readonly struct EmuPoint
+    {
+        /// <summary>
+        /// 创建 <see cref="Emu"/> 表示的点
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        public EmuPoint(Emu x, Emu y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        /// <summary>
+        /// 表示 X 坐标
+        /// </summary>
+        public Emu X { get; }
+
+        /// <summary>
+        /// 表示 Y 坐标
+        /// </summary>
+        public Emu Y { get; }
+    }
+}
