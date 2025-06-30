a726c59314e341f6cfb66316f010490af9364cb8
lindexi <lindexi_gd@163.com>
2025-06-30 17:35:27 +0800

    测试多线程处置的方法

--- src/MediaConverters/MediaConverters.Tests/Imaging/Effect/SoftEdgeHelperTests.cs
+++ src/MediaConverters/MediaConverters.Tests/Imaging/Effect/SoftEdgeHelperTests.cs
@@ -1,6 +1,10 @@
-﻿using DotNetCampus.MediaConverters.Imaging.Effect;
+﻿using System.Numerics;
+
+using DotNetCampus.MediaConverters.Imaging.Effect;
+
 using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
 
 namespace DotNetCampus.MediaConverters.Tests.Imaging.Effect;
 
@@ -10,14 +14,35 @@ public class SoftEdgeHelperTests
     [TestMethod()]
     public void SetSoftEdgeMaskTest()
     {
-        Image<Rgba32> bitmap = TestFileProvider.GetDefaultTestImage();
-        var alphaRepresentation = bitmap.PixelType.AlphaRepresentation;
+        Image<Rgba32> image = TestFileProvider.GetDefaultTestImage();
 
-        SoftEdgeHelper.SetSoftEdgeMask(bitmap, 50.0f);
+        SoftEdgeHelper.SetSoftEdgeMask(image, 50.0f);
 
-        var file = bitmap.SaveAsTestImageFile();
+        var file = image.SaveAsTestImageFile();
 
         Assert.IsTrue(File.Exists(file));
+        TestHelper.OpenFileInExplorer(file);
+    }
+
+    [TestMethod()]
+    public void TestManipulation()
+    {
+        Image<Rgba32> image = TestFileProvider.GetDefaultTestImage();
+        image.Mutate(c => c.ProcessPixelRowsAsVector4(row =>
+        {
+            for (int col = 0; col < row.Length; col++)
+            {
+                // We can apply any custom processing logic here
+                ref var pixel = ref row[col];
+                pixel = Vector4.SquareRoot(pixel);
+                if (col < row.Length - 1)
+                {
+                    pixel = pixel * (1 + Vector4.DistanceSquared(pixel, row[col + 1]/100));
+                }
+            }
+        }));
+
+        var file = image.SaveAsTestImageFile();
         TestHelper.OpenFileInExplorer(file);
     }
 }
