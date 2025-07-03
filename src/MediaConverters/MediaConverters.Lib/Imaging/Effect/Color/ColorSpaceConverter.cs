f4bfc3e2cefc129749b1c8ca77b9a753f273b1a1
lindexi <lindexi_gd@163.com>
2025-07-03 15:22:13 +0800

    添加颜色空间算法

---
+++ src/MediaConverters/MediaConverters.Lib/Imaging/Effect/Color/ColorSpaceConverter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetCampus.MediaConverters.Imaging.Effect.Color;
+
+/// <summary>
+/// 色彩空间转换器
+/// </summary>
+internal static class ColorSpaceConverter
+{
+    /// <summary>
+    /// 将SRgb颜色转为CIE_XYZ
+    /// https://en.wikipedia.org/wiki/SRGB#The_forward_transformation_.28CIE_xyY_or_CIE_XYZ_to_sRGB.29
+    /// </summary>
+    /// <param name="sRgb">(单通道)颜色值 范围0-1</param>
+    /// <returns></returns>
+    public static float SRgbToCIE_XYZ(float sRgb)
+    {
+        if (sRgb <= 04045) return sRgb / 12.92f;
+        return (float) Math.Pow((sRgb + 055f) / 155f, 2.4f);
+    }
+
+    /// <summary>
+    /// 将CIE_XYZ转为SRgb颜色
+    /// https://en.wikipedia.org/wiki/SRGB#The_forward_transformation_.28CIE_xyY_or_CIE_XYZ_to_sRGB.29
+    /// </summary>
+    /// <param name="linearRgb">(单通道)颜色值 范围0-1</param>
+    /// <returns></returns>
+    public static float CIE_XYZToSRgb(float linearRgb)
+    {
+        if (linearRgb < 0031308) return 12.92f * linearRgb;
+
+        //var linearR=3.24096994*sR-1.53738318*sg-0.49861076*sb
+        return (float) Math.Pow(linearRgb, 1 / 2.4) * 155f - 055f;
+    }
+
+}
---
+++ src/MediaConverters/MediaConverters.Lib/Imaging/Effect/Extensions/BitmapExtension.cs
@@ -0,0 +1,146 @@
+﻿using DotNetCampus.MediaConverters.Imaging.Effect.Color;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+
+namespace DotNetCampus.MediaConverters.Imaging.Effect.Extensions;
+
+/// <summary>
+/// <see cref="Image{Rgba32}"/>拓展
+/// </summary>
+public static class BitmapExtension
+{
+    #region 逐像素处理
+
+    /// <summary>
+    /// 将<see cref="Image{Rgba32}"/>逐像素处理
+    /// </summary>
+    /// <returns></returns>
+    public static void PerPixelProcess(this Image<Rgba32> bitmap, Func<ColorMetadata, ColorMetadata> func)
+    {
+        bitmap.ProcessPixelRows(accessor =>
+        {
+            for (int rowIndex = 0; rowIndex < accessor.Height; rowIndex++)
+            {
+                var span = accessor.GetRowSpan(rowIndex);
+                for (int colIndex = 0; colIndex < span.Length; colIndex++)
+                {
+                    //获取颜色
+                    var color = new ColorMetadata(span[colIndex]);
+                    //处理颜色
+                    var targetColor = func(color);
+                    //保存颜色
+                    span[colIndex] = targetColor.Color;
+                }
+            }
+        });
+
+        //var pixelFormat = bitmap.PixelFormat;
+
+        //if (pixelFormat != PixelFormat.Format32bppArgb && pixelFormat != PixelFormat.Format24bppRgb)
+        //{
+        //    throw new NotSupportedException($"Unsupported image pixel format {nameof(pixelFormat)} is used.");
+        //}
+
+        //var cols = bitmap.Width;
+        //var rows = bitmap.Height;
+        //var channels = Image.GetPixelFormatSize(bitmap.PixelFormat) / 8;
+        //var total = cols * rows * channels;
+
+        ////锁定图片并拷贝图片像素
+        //var rect = new Rectangle(0, 0, cols, rows);
+        //var bitmapData = bitmap.LockBits(rect, ImageLockMode.ReadWrite, bitmap.PixelFormat);
+        //var iPtr = bitmapData.Scan0;
+        //var data = new byte[total];
+        //Marshal.Copy(iPtr, data, 0, total);
+
+        ////逐像素处理
+        //Parallel.For(0, rows, row =>
+        //{
+        //    for (var col = 0; col < cols; col++)
+        //    {
+        //        var indexOffset = (row * cols + col) * channels;
+        //        var sourceColorMetadata = CreateColorFromData(data, indexOffset, channels);
+        //        var targetColorMetadata = func(sourceColorMetadata);
+        //        SaveToData(targetColorMetadata, data, indexOffset, channels);
+        //    }
+        //});
+
+        //Marshal.Copy(data, 0, iPtr, total);
+        //bitmap.UnlockBits(bitmapData);
+    }
+
+    /// <summary>
+    ///     尝试获取颜色
+    /// </summary>
+    /// <param name="colorData"></param>
+    /// <param name="offset"></param>
+    /// <param name="channels"></param>
+    /// <returns></returns>
+    private static ColorMetadata CreateColorFromData(byte[] colorData, int offset, int channels)
+    {
+        //需要考虑大小端
+        var isLittleEndian = BitConverter.IsLittleEndian;
+
+        if (channels == 3)
+        {
+            var r = isLittleEndian ? colorData[offset + 2] : colorData[offset];
+            var g = colorData[offset + 1];
+            var b = isLittleEndian ? colorData[offset] : colorData[offset + 2];
+            return new ColorMetadata(r / 255f, g / 255f, b / 255f);
+        }
+
+        if (channels == 4)
+        {
+            var a = isLittleEndian ? colorData[offset + 3] : colorData[offset + 0];
+            var r = isLittleEndian ? colorData[offset + 2] : colorData[offset + 1];
+            var g = isLittleEndian ? colorData[offset + 1] : colorData[offset + 2];
+            var b = isLittleEndian ? colorData[offset + 0] : colorData[offset + 3];
+            return new ColorMetadata(r / 255f, g / 255f, b / 255f, a / 255f);
+        }
+
+        return new ColorMetadata(0 / 255f, 0 / 255f, 0 / 255f);
+    }
+
+    /// <summary>
+    ///     保存颜色到数组
+    /// </summary>
+    /// <param name="color"></param>
+    /// <param name="colorData"></param>
+    /// <param name="offset"></param>
+    /// <param name="channels"></param>
+    /// <returns></returns>
+    private static void SaveToData(ColorMetadata color, byte[] colorData, int offset, int channels)
+    {
+        //需要考虑大小端
+        var isLittleEndian = BitConverter.IsLittleEndian;
+
+        var r = color.ARGB8bit.Item1;
+        var g = color.ARGB8bit.Item2;
+        var b = color.ARGB8bit.Item3;
+        var a = color.ARGB8bit.Item4;
+
+        if (channels == 3)
+        {
+            colorData[offset] = isLittleEndian ? b : r;
+            colorData[offset + 1] = g;
+            colorData[offset + 2] = isLittleEndian ? r : b;
+        }
+
+        if (channels == 4)
+        {
+            colorData[offset + 0] = isLittleEndian ? b : a;
+            colorData[offset + 1] = isLittleEndian ? g : r;
+            colorData[offset + 2] = isLittleEndian ? r : g;
+            colorData[offset + 3] = isLittleEndian ? a : b;
+        }
+    }
+
+    #endregion
+}
