c5f6e95302b2bafa78eab2e34ef39d4e5620215b
lindexi <lindexi_gd@163.com>
2025-07-04 11:49:57 +0800

    添加一些上下文

---
+++ src/MediaConverters/MediaConverters.Tool/Contexts/ErrorCode.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetCampus.MediaConverter.Contexts;
+
+internal readonly record struct ErrorCode
+{
+    /// <summary>
+    /// 成功
+    /// </summary>
+    public static readonly ErrorCode Success = new(0, "Success");
+
+    /// <summary>
+    /// 未知错误
+    /// </summary>
+    public static readonly ErrorCode UnknownError = new(-1, "Unknown error");
+
+    /// <summary>
+    /// 错误代码
+    /// </summary>
+    public int Code { get; init; }
+
+    /// <summary>
+    /// 错误信息
+    /// </summary>
+    public string Message { get; init; }
+
+    public ErrorCode(int code, string message)
+    {
+        Code = code;
+        Message = message;
+
+        CodeList ??= [];
+        CodeList.Add(this);
+    }
+
+    private static List<ErrorCode>? CodeList { get; set; }
+}
