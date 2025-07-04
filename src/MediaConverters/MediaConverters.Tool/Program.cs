e65a5353dda691b49578084f894ea9c40a0eed1d
lindexi <lindexi_gd@163.com>
2025-07-04 15:27:52 +0800

    测试限制面积的算法

--- src/MediaConverters/MediaConverters.Tool/Program.cs
+++ src/MediaConverters/MediaConverters.Tool/Program.cs
@@ -12,6 +12,20 @@ using System.Text;
 
 ImageDecoder d = WebpDecoder.Instance;
 
+var maxPixelCount = 100_00_00;
+
+var w = 1500;
+var h = 1300;
+
+var s = w / (double) h;
+
+var pw = (int) Math.Sqrt(maxPixelCount * w / (double) h);
+var ph = (int) Math.Sqrt(maxPixelCount * h / (double) w);
+
+var s1 = pw / (double) ph;
+
+
+
 var tiffFile = @"E:\Download\file_example_TIFF_1MB.tiff";
 var file = @"E:\Download\file_example_favicon.ico";
 var buffer = File.ReadAllBytes(file);
