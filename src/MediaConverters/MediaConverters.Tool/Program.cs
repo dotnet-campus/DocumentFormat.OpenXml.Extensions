0b1ba595f04d1c6bfff9ffe8ab5801a48efbe816
lindexi <lindexi_gd@163.com>
2025-07-04 11:46:13 +0800

    测试和获取本质的图片格式

--- src/MediaConverters/MediaConverters.Tool/Program.cs
+++ src/MediaConverters/MediaConverters.Tool/Program.cs
@@ -2,13 +2,29 @@
 
 using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.Formats;
+using SixLabors.ImageSharp.Formats.Bmp;
+using SixLabors.ImageSharp.Formats.Tiff;
 using SixLabors.ImageSharp.Formats.Webp;
 using SixLabors.ImageSharp.PixelFormats;
 
 ImageDecoder d = WebpDecoder.Instance;
 
+var tiffFile = @"E:\Download\file_example_TIFF_1MB.tiff";
 var file = @"E:\Download\file_example_favicon.ico";
 var buffer = File.ReadAllBytes(file);
+
+var tiffImageFormatDetector = new TiffImageFormatDetector();
+if (tiffImageFormatDetector.TryDetectFormat(File.ReadAllBytes(tiffFile),out var f))
+{
+    
+}
+
+var detector = new BmpImageFormatDetector();
+if (detector.TryDetectFormat(buffer,out var format))
+{
+    
+}
+
 var detectFormat = Image.DetectFormat(buffer);
 // ImageFormatManager.ThrowInvalidDecoder(configuration.ImageFormatsManager);
 var imageInfo = Image.Identify(buffer);
