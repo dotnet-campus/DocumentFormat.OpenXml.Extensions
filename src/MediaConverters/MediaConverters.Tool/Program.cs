0cd53cbf89d1f9cfd29ac6d5b8e576efd9bfd8d9
lindexi <lindexi_gd@163.com>
2025-07-04 11:21:39 +0800

    测试图片格式

--- src/MediaConverters/MediaConverters.Tool/Program.cs
+++ src/MediaConverters/MediaConverters.Tool/Program.cs
@@ -1,2 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Formats;
+using SixLabors.ImageSharp.Formats.Webp;
+
+ImageDecoder d = WebpDecoder.Instance;
+
+foreach (IImageFormat imageFormat in Configuration.Default.ImageFormats)
+{
+    Console.WriteLine(imageFormat.Name);
+}
+
+/*
+   PNG
+   JPEG
+   GIF
+   BMP
+   PBM
+   TGA
+   TIFF
+   Webp
+   QOI
+ */
 Console.WriteLine("Hello, World!");
