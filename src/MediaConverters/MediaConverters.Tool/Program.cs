bf79ac1839b04d556c5535a7c0414410ce29dc91
lindexi <lindexi_gd@163.com>
2025-07-04 14:53:11 +0800

    测试加载不支持的图片

--- src/MediaConverters/MediaConverters.Tool/Program.cs
+++ src/MediaConverters/MediaConverters.Tool/Program.cs
@@ -8,15 +8,25 @@ using SixLabors.ImageSharp.Formats.Webp;
 using SixLabors.ImageSharp.PixelFormats;
 using SixLabors.ImageSharp.Processing;
 
+using System.Text;
+
 ImageDecoder d = WebpDecoder.Instance;
 
 var tiffFile = @"E:\Download\file_example_TIFF_1MB.tiff";
-var file = @"E:\Download\file_example_JPG_100kB.jpg";
+var file = @"E:\Download\file_example_favicon.ico";
 var buffer = File.ReadAllBytes(file);
 
-var image = Image.Load<Rgba32>(buffer);
-image.Mutate(context => context.Resize(new Size(100, 100), compand: true));
-Console.WriteLine(image.Width);
-image.SaveAsPng("1.png");
+try
+{
+    var image = Image.Load<Rgba32>(buffer);
+    image.Mutate(context => context.Resize(new Size(100, 100), compand: true));
+    Console.WriteLine(image.Width);
+    image.SaveAsPng("1.png");
+
+    
+}
+catch (ImageFormatException e)
+{
+}
 
 Console.WriteLine("Hello, World!");
