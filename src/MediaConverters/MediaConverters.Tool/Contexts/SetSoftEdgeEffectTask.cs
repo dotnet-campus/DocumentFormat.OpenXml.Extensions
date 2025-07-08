e1d0bc4e149c1136d144da03230541b911cefba4
lindexi <lindexi_gd@163.com>
2025-07-08 16:22:18 +0800

    设置柔化边缘时，带上对应的尺寸信息

--- src/MediaConverters/MediaConverters.Tool/Contexts/SetSoftEdgeEffectTask.cs
+++ src/MediaConverters/MediaConverters.Tool/Contexts/SetSoftEdgeEffectTask.cs
@@ -6,4 +6,14 @@
 public class SetSoftEdgeEffectTask : IImageConvertTask
 {
     public float Radius { get; init; }
+
+    /// <summary>
+    /// 要求的图片宽度，如果不指定则使用原图宽度
+    /// </summary>
+    public int? ImageWidth { get; init; }
+
+    /// <summary>
+    /// 要求的图片高度，如果不指定则使用原图高度
+    /// </summary>
+    public int? ImageHeight { get; init; }
 }
--- src/MediaConverters/MediaConverters.Tool/Workers/SetSoftEdgeEffectWorker.cs
+++ src/MediaConverters/MediaConverters.Tool/Workers/SetSoftEdgeEffectWorker.cs
@@ -2,6 +2,7 @@
 using DotNetCampus.MediaConverters.Imaging.Effects;
 using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
 
 namespace DotNetCampus.MediaConverters.Workers;
 
@@ -9,6 +10,13 @@ internal class SetSoftEdgeEffectWorker : WorkerBase<SetSoftEdgeEffectTask>
 {
     protected override void RunCore(Image<Rgba32> image, SetSoftEdgeEffectTask task)
     {
+        if (task.ImageWidth is not null || task.ImageHeight is not null)
+        {
+            var width = task.ImageWidth ?? image.Width;
+            var height = task.ImageHeight ?? image.Height;
+            image.Mutate(context => context.Resize(width, height));
+        }
+
         image.SetSoftEdgeEffect(task.Radius);
     }
 }
